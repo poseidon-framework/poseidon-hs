90bd062ccaea669e0d255f0aa599dd7094adbd74
Stephan Schiffels <stephan_schiffels@mac.com>
2020-08-26 16:53:24 +0200

    implemented raw output for list command

--- src-executables/Main.hs
+++ src-executables/Main.hs
@@ -1,17 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Control.Monad       (forM, forM_, when)
-import           Data.List           (intercalate, isInfixOf, groupBy, sortOn, nub)
+import           Data.List           (groupBy, intercalate, isInfixOf, nub,
+                                      sortOn)
 import           Data.Text           (unpack)
 import           Data.Version        (showVersion)
 import           Options.Applicative as OP
-import System.IO (hPutStrLn, stderr)
 import           Poseidon.Package    (EigenstratIndEntry (..),
                                       PoseidonPackage (..),
                                       filterDuplicatePackages,
                                       findPoseidonPackages, getIndividuals)
-import           Text.Layout.Table   (asciiRoundS, column, def, expand, rowsG,
-                                      tableString, titlesH, expandUntil)
-            
+import           System.IO           (hPutStrLn, stderr)
+import           Text.Layout.Table   (asciiRoundS, column, def, expand,
+                                      expandUntil, rowsG, tableString, titlesH)
+
 
 data Options = CmdList ListOptions
     | CmdFstats FstatsOptions
@@ -19,6 +20,7 @@ data Options = CmdList ListOptions
 data ListOptions = ListOptions
     { _loBaseDirs   :: [FilePath]
     , _loListEntity :: ListEntity
+    , _loRawOutput  :: Bool
     }
 
 data ListEntity = ListPackages
@@ -52,7 +54,7 @@ optParser = OP.subparser $
         (OP.progDesc "fstat: running fstats")
 
 listOptParser :: OP.Parser ListOptions
-listOptParser = ListOptions <$> parseBasePaths <*> parseListEntity
+listOptParser = ListOptions <$> parseBasePaths <*> parseListEntity <*> parseRawOutput
 
 fstatsOptParser :: OP.Parser FstatsOptions
 fstatsOptParser = FstatsOptions <$> parseBasePaths
@@ -70,42 +72,56 @@ parseListEntity = parseListPackages <|> parseListGroups <|> parseListIndividuals
     parseListGroups = OP.flag' ListGroups (OP.long "groups" <> OP.help "list groups")
     parseListIndividuals = OP.flag' ListIndividuals (OP.long "individuals" <> OP.help "list individuals")
 
+parseRawOutput :: OP.Parser Bool
+parseRawOutput = OP.switch (OP.long "raw" <> OP.short 'r' <> OP.help "output table as tsv without header. Useful for pipeing into group")
+
 runList :: ListOptions -> IO ()
-runList (ListOptions baseDirs listEntity) = do
+runList (ListOptions baseDirs listEntity rawOutput) = do
     packages <- getPackages $ baseDirs
     hPutStrLn stderr $ (show . length $ packages) ++ " packages found"
-    case listEntity of
+    (tableH, tableB) <- case listEntity of
         ListPackages -> do
             let tableH = ["Title", "Date", "Nr Individuals"]
             tableB <- forM packages $ \pac -> do
                 inds <- getIndividuals pac
                 return [posPacTitle pac, showMaybeDate (posPacLastModified pac), show (length inds)]
-            let colSpecs = replicate 3 (column expand def def def)
-            putStrLn $ tableString colSpecs asciiRoundS (titlesH tableH) [rowsG tableB]
+            return (tableH, tableB)
         ListGroups -> do
             allInds <- fmap concat . forM packages $ \pac -> do
                 inds <- getIndividuals pac
                 return [[posPacTitle pac, name, pop] | (EigenstratIndEntry name _ pop) <- inds]
             let allIndsSortedByGroup = groupBy (\a b -> a!!2 == b!!2) . sortOn (!!2) $ allInds
-                tableBody = do
+                tableB = do
                     indGroup <- allIndsSortedByGroup
                     let packages = nub [i!!0 | i <- indGroup]
                     let nrInds = length indGroup
                     return [(indGroup!!0)!!2, intercalate "," packages, show nrInds]
             let tableH = ["Group", "Packages", "Nr Individuals"]
-                colSpecs = replicate 3 (column (expandUntil 50) def def def)
-            putStrLn $ tableString colSpecs asciiRoundS (titlesH tableH) [rowsG tableBody]
+            return (tableH, tableB)
         ListIndividuals -> do
-            fullTable <- fmap concat . forM packages $ \pac -> do
+            tableB <- fmap concat . forM packages $ \pac -> do
                 inds <- getIndividuals pac
                 return [[posPacTitle pac, name, pop] | (EigenstratIndEntry name _ pop) <- inds]
-            hPutStrLn stderr ("found " ++ show (length fullTable) ++ " individuals.")
+            hPutStrLn stderr ("found " ++ show (length tableB) ++ " individuals.")
             let tableH = ["Package", "Individual", "Population"]
-                colSpecs = replicate 3 (column expand def def def)
-            putStrLn $ tableString colSpecs asciiRoundS (titlesH tableH) [rowsG fullTable]
+            return (tableH, tableB)
+      
+    if rawOutput then
+        putStrLn $ intercalate "\n" [intercalate "\t" row | row <- tableB]
+    else
+        case listEntity of
+            ListPackages -> do
+                let colSpecs = replicate 3 (column expand def def def)
+                putStrLn $ tableString colSpecs asciiRoundS (titlesH tableH) [rowsG tableB]
+            ListGroups -> do
+                let colSpecs = replicate 3 (column (expandUntil 50) def def def)
+                putStrLn $ tableString colSpecs asciiRoundS (titlesH tableH) [rowsG tableB]
+            ListIndividuals -> do
+                let colSpecs = replicate 3 (column expand def def def)
+                putStrLn $ tableString colSpecs asciiRoundS (titlesH tableH) [rowsG tableB]
   where
     showMaybeDate (Just d) = show d
-    showMaybeDate Nothing = "n/a"
+    showMaybeDate Nothing  = "n/a"
 
 
 runFstats :: FstatsOptions -> IO ()
