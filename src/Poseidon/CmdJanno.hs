b361df32ddc9c195d18dd49576dad2bc4fd68d17
Clemens Schmid <clemens@nevrome.de>
2020-11-05 19:07:01 +0100

    long experiments with bytestring manipulation

--- src/Poseidon/CmdJanno.hs
+++ src/Poseidon/CmdJanno.hs
@@ -4,7 +4,7 @@
 module Poseidon.CmdJanno (runJanno, JannoOptions(..)) where
 
 import           Poseidon.Package          (PoseidonSample(..))
-import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Csv as Csv
 import           Data.Vector (Vector)
 import qualified Data.Vector as V
@@ -22,13 +22,25 @@ decodingOptions = Csv.defaultDecodeOptions {
 
 instance Csv.FromRecord PoseidonSample
 
+-- replace :: B.ByteString -> B.ByteString -> B.ByteString -> B.ByteString
+-- replace from to s =
+--    let (h, t) = B.breakSubstring from s
+--    in  B.concat [h, to, B.drop (B.length from) t]
+
+-- replaceNA :: B.ByteString -> B.ByteString
+-- replaceNA jannoFile =
+--    let na = B.pack "n/a" in
+--    replace na B.empty jannoFile
+
 -- | The main function running the janno command
 runJanno :: JannoOptions -> IO ()
 runJanno (JannoOptions jannoPath) = do 
-    jannoFile <- BL.readFile jannoPath
+    jannoFile <- B.readFile jannoPath
+    -- replace n/a with empty
+    -- let jannoFile = replaceNA jannoFile
 
     case Csv.decodeWith decodingOptions Csv.HasHeader jannoFile of
         Left err -> do
-            putStrLn ("Unable to parse data: " ++ err)
+            Prelude.putStrLn ("Unable to parse data: " ++ err)
         Right (poseidonSamples :: Vector PoseidonSample) -> do
-            putStrLn ("Parsed janno file: data for " ++ show (V.length poseidonSamples) ++ " poseidonSamples")
+            Prelude.putStrLn ("Parsed janno file: data for " ++ show (V.length poseidonSamples) ++ " poseidonSamples")
