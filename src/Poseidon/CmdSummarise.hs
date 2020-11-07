42e51f6331833a7e47ea8127ad969850702b0393
Clemens Schmid <clemens@nevrome.de>
2020-11-07 22:40:05 +0100

    more features for summarise: mean age

--- src/Poseidon/CmdSummarise.hs
+++ src/Poseidon/CmdSummarise.hs
@@ -18,19 +18,31 @@ data SummariseOptions = SummariseOptions
 pasteFirst3 :: [String] -> String
 pasteFirst3 [] = "no values"
 pasteFirst3 xs = 
-    (L.intercalate ", " $ take 3 xs) ++ if (length xs > 3) then ", ..." else ""
+    (L.intercalate ", " $ take 2 xs) ++ if (length xs > 2) then ", ..." else ""
 
 removeNothing :: [Maybe a] -> [a]
 removeNothing xs =
     let onlyJust = filter DM.isJust xs
     in DM.catMaybes onlyJust
 
+avg :: [Double] -> Double
+avg xs = let sum = L.foldl' (+) 0 xs
+         in sum / (fromIntegral $ length xs)
+
+roundTo :: Int -> Double -> Double
+roundTo n x = (fromIntegral (floor (x * t))) / t
+    where t = 10^n
+
+avgRoundTo :: Int -> [Double] -> Double
+avgRoundTo n xs = roundTo n $ avg xs
+
 summarisePoseidonSamples :: [PoseidonSample] -> IO ()
 summarisePoseidonSamples xs = do
-    putStrLn $ "Number of samples: " ++ (show $ length xs)
-    putStrLn $ "Individuals: " ++ pasteFirst3 (map posSamIndividualID xs)
-    putStrLn $ "Populations: " ++ pasteFirst3 (map head (map posSamGroupName xs))
-    putStrLn $ "Countries: " ++ pasteFirst3 (removeNothing (map posSamCountry xs))
+    putStrLn $ "Number of samples:\t" ++ (show $ length xs)
+    putStrLn $ "Individuals:\t\t" ++ pasteFirst3 (map posSamIndividualID xs)
+    putStrLn $ "Populations:\t\t" ++ pasteFirst3 (L.nub $ map head (map posSamGroupName xs))
+    putStrLn $ "Countries:\t\t" ++ pasteFirst3 (L.nub $ removeNothing $ map posSamCountry xs)
+    putStrLn $ "Mean age BC/AD:\t\t" ++ show (avgRoundTo 0 $ map fromIntegral (removeNothing $ map posSamDateBCADMedian xs))
 
 -- | The main function running the janno command
 runSummarise :: SummariseOptions -> IO ()
