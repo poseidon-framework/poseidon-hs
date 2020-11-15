39fa0457b495478b28018e266a16b8beaf12a545
Clemens Schmid <clemens@nevrome.de>
2020-11-15 11:28:48 +0100

    janno-bibtex overlap check

--- src/Poseidon/CmdValidate.hs
+++ src/Poseidon/CmdValidate.hs
@@ -14,7 +14,7 @@ import           Data.Maybe         (mapMaybe)
 import           System.IO          (hPutStrLn, stderr)
 import           Text.CSL.Exception (renderError)
 import           Text.CSL.Reference (refId, unLiteral)
-import           Data.List          (nub)
+import           Data.List          (nub, (\\), intercalate)
 import           Data.Text          (unpack)
 
 -- | A datatype representing command line options for the validate command
@@ -54,12 +54,14 @@ runValidate (ValidateOptions baseDirs) = do
         ++ " literature references seem to be fine"
     --
     -- Cross-file consistency
-    putStrLn "JANNO-BIBTEX overlap:"
+    putStrLn "JANNO-BIBTEX interaction:"
     let literatureInJanno = nub $ mapMaybe posSamPublication jannoSamples
     let literatureInBib = nub $ map (unpack . unLiteral . refId) bibReferences
-    print literatureInJanno
-    print literatureInBib
-    putStrLn "huhu"
+    let literatureNotInBibButInJanno = literatureInJanno \\ literatureInBib
+    if null literatureNotInBibButInJanno
+        then putStrLn "All literature in the .janno files has BibTeX entries"
+        else putStrLn $ "The following papers lack BibTeX entries: " ++ 
+                        intercalate ", " literatureNotInBibButInJanno
     --
     -- Final report: Error code generation
     -- ...
