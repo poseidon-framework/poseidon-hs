43705cb5466b7fb1b4af8ca3abfbef1d5af24771
Clemens Schmid <clemens@nevrome.de>
2020-11-15 11:03:29 +0100

    extracting literature keys as strings

--- src/Poseidon/CmdValidate.hs
+++ src/Poseidon/CmdValidate.hs
@@ -5,13 +5,17 @@ module Poseidon.CmdValidate (runValidate, ValidateOptions(..)) where
 import           Poseidon.Package   (loadPoseidonPackages,
                                     loadJannoFiles,
                                     PoseidonPackage(..),
+                                    PoseidonSample(..),
                                     loadBibTeXFiles)
 import           Poseidon.Utils     (printPoseidonJannoException)
 
 import qualified Data.Either        as E
-import           Data.Maybe         (catMaybes)
+import           Data.Maybe         (mapMaybe)
 import           System.IO          (hPutStrLn, stderr)
 import           Text.CSL.Exception (renderError)
+import           Text.CSL.Reference (refId, unLiteral)
+import           Data.List          (nub)
+import           Data.Text          (unpack)
 
 -- | A datatype representing command line options for the validate command
 data ValidateOptions = ValidateOptions
@@ -21,25 +25,27 @@ data ValidateOptions = ValidateOptions
 -- | The main function running the janno command
 runValidate :: ValidateOptions -> IO ()
 runValidate (ValidateOptions baseDirs) = do
-    --
+    -- POSEIDON.yml
     putStrLn "POSEIDON.yml file consistency:"
     packages <- loadPoseidonPackages baseDirs
     hPutStrLn stderr $ (show . length $ packages) 
         ++ " Poseidon packages seem to be fine"
-    --
+    -- JANNO
     putStrLn "JANNO file consistency:"
     let jannoFilePaths = map posPacJannoFile packages
     let jannoFiles = loadJannoFiles jannoFilePaths
-    jannoSamples <- fmap concat jannoFiles
-    mapM_ printPoseidonJannoException (E.lefts jannoSamples)
-    hPutStrLn stderr $ (show . length $ E.rights jannoSamples) 
+    jannoSamplesRaw <- fmap concat jannoFiles
+    let jannoSamples = E.rights jannoSamplesRaw
+    let jannoExceptions = E.lefts jannoSamplesRaw
+    mapM_ printPoseidonJannoException jannoExceptions
+    hPutStrLn stderr $ (show . length $ jannoSamples) 
         ++ " samples seem to be fine"
     --
     -- Genotype file consistency (if available and without loading them completely!)
     -- ...
-    -- 
+    -- BIBTEX
     putStrLn "BIBTEX file consistency:"
-    let bibFilePaths = catMaybes $ map posPacBibFile packages
+    let bibFilePaths = mapMaybe posPacBibFile packages
     let bibFiles = loadBibTeXFiles bibFilePaths
     bibReferences <- fmap (concat . E.rights) bibFiles
     bibExceptions <- fmap E.lefts bibFiles
@@ -48,7 +54,12 @@ runValidate (ValidateOptions baseDirs) = do
         ++ " literature references seem to be fine"
     --
     -- Cross-file consistency
-    -- ...
+    putStrLn "JANNO-BIBTEX overlap:"
+    let literatureInJanno = nub $ mapMaybe posSamPublication jannoSamples
+    let literatureInBib = nub $ map (unpack . unLiteral . refId) bibReferences
+    print literatureInJanno
+    print literatureInBib
+    putStrLn "huhu"
     --
     -- Final report: Error code generation
     -- ...
