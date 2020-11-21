7acdd054c1695c9eef8e8d5daed3bfb781b38fa4
Clemens Schmid <clemens@nevrome.de>
2020-11-21 22:44:08 +0100

    create output directory

--- src/Poseidon/CmdMerge.hs
+++ src/Poseidon/CmdMerge.hs
@@ -10,7 +10,10 @@ import           Poseidon.Package       (PoseidonPackage(..),
                                         PoseidonSample(..),
                                         GenotypeDataSpec(..))
 import           Data.Maybe             (catMaybes, isJust)
-import           System.IO              (hPutStrLn, stderr)  
+import           System.IO              (hPutStrLn, stderr)
+import           System.FilePath        ((</>))
+import           System.Directory       (createDirectory)
+import           Control.Monad          (when)
 
 -- | A datatype representing command line options for the survey command
 data MergeOptions = MergeOptions
@@ -28,6 +31,10 @@ runMerge (MergeOptions baseDirs outPath) = do
     jannoFiles <- maybeLoadJannoFiles packages
     let jannoMaybeList = jannoToSimpleMaybeList jannoFiles
     let anyJannoIssues = not $ all isJust jannoMaybeList
-    -- merge
-    writeJannoFile outPath (concat $ catMaybes jannoMaybeList)
-    
+    let goodJannoRows = concat $ catMaybes jannoMaybeList
+    -- create new package
+    createDirectory outPath
+    writeJannoFile (outPath </> "test.janno") goodJannoRows
+        -- print read issue warning
+    when anyJannoIssues $
+        putStrLn "\nThere were issues with incomplete, missing or invalid data. Run poet validate to learn more."
