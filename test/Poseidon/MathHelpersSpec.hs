3b491d42efc9e53377c4207aeed19e40bb449d15
Clemens Schmid <clemens@nevrome.de>
2020-12-21 16:21:39 +0100

    minimal test for roundTo

--- test/Poseidon/MathHelpersSpec.hs
+++ test/Poseidon/MathHelpersSpec.hs
@@ -9,14 +9,23 @@ import           Test.QuickCheck
 
 spec = do
     testAvg
+    testRoundTo
 
 testAvg :: Spec
 testAvg = 
     describe "Poseidon.MathHelpers.avg" $ do
     it "should always be within the range of the input list" $ property test
-        where 
-            test :: [Double] -> Bool
-            test [] = True 
-            test x = minimum x <= avg x && maximum x >= avg x
-                       
+    where 
+        test :: [Double] -> Bool
+        test [] = True 
+        test x = minimum x <= avg x && maximum x >= avg x
+
+testRoundTo :: Spec
+testRoundTo = 
+    describe "Poseidon.MathHelpers.roundTo" $ do
+    it "should always round to the desired precision" $ property test
+    where
+        test :: Int -> Double -> Bool
+        test _ x = abs (x - roundTo 3 x ) < 0.001
+
 
