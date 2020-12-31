f0ffec6df98e2bc2fa612264e902de65bf713a55
Clemens Schmid <clemens@nevrome.de>
2020-12-31 16:39:02 +0100

    test template for ValidateSpec

---
+++ test/Poseidon/ValidateSpec.hs
@@ -0,0 +1,21 @@
+module Poseidon.ValidateSpec (spec) where
+
+import Poseidon.CLI.Validate ()
+
+import           Test.Hspec
+
+spec = do
+    testRenderMismatch
+    testZipWithPadding
+
+testRenderMismatch :: Spec
+testRenderMismatch = 
+    describe "Poseidon.CLI.Validate.renderMismatch" $ do
+    it "should" $ do
+        1 `shouldBe` 1
+
+testZipWithPadding :: Spec
+testZipWithPadding = 
+    describe "Poseidon.CLI.Validate.zipWithPadding" $ do
+    it "should" $ do
+        1 `shouldBe` 1
