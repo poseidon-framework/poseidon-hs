cb18340fcda5ef66906ce66923e23059f906ae24
Clemens Schmid <clemens@nevrome.de>
2021-01-19 17:39:36 +0100

    finished the significant improvement of summarise - shows now counts for grouping variables in descending order

--- src/Poseidon/CLI/Summarise.hs
+++ src/Poseidon/CLI/Summarise.hs
@@ -8,7 +8,7 @@ import           Poseidon.Package       (loadPoseidonPackages, maybeLoadJannoFil
 
 import           Control.Monad          (when)
 import           Data.List              (sortBy, nub, group, sort, intercalate)
-import           Data.Maybe             (catMaybes, isJust, mapMaybe)
+import           Data.Maybe             (catMaybes, isJust, mapMaybe, fromJust)
 import           System.IO              (hPutStrLn, stderr)
 import           Text.Layout.Table      (asciiRoundS, column, def, expand,
                                          rowsG, tableString, titlesH, expandUntil)
@@ -44,16 +44,16 @@ summarisePoseidonSamples xs rawOutput = do
                 ["Nr Individuals", show (length xs)],
                 ["Individuals", paste $ sort $ map posSamIndividualID xs],
                 ["Nr Groups", show $ length $ nub $ map posSamGroupName xs],
-                ["Groups", printFrequency ", " $ frequency $ map (head . posSamGroupName) xs],
+                ["Groups", printFrequencyString ", " $ frequency $ map (head . posSamGroupName) xs],
                 ["Nr Publications", show $ length $ nub $ map posSamPublication xs],
-                ["Publications", printFrequencyMaybe ", " $ frequency $ map posSamPublication xs],
+                ["Publications", printFrequencyMaybeString ", " $ frequency $ map posSamPublication xs],
                 ["Nr Countries", show $ length $ nub $ map posSamCountry xs],
-                ["Countries", printFrequencyMaybe ", " $ frequency $ map posSamCountry xs],
+                ["Countries", printFrequencyMaybeString ", " $ frequency $ map posSamCountry xs],
                 ["Mean age BC/AD", meanAndSdInteger $ map fromIntegral $ mapMaybe posSamDateBCADMedian xs],
                 ["Dating type", printFrequencyMaybe ", " $ frequency $ map posSamDateType xs],
                 ["Sex distribution", printFrequency ", " $ frequency $ map posSamGeneticSex xs],
-                ["MT haplogroups", printFrequencyMaybe ", " $ frequency $ map posSamMTHaplogroup xs],
-                ["Y haplogroups",printFrequencyMaybe ", " $ frequency $ map posSamYHaplogroup xs],
+                ["MT haplogroups", printFrequencyMaybeString ", " $ frequency $ map posSamMTHaplogroup xs],
+                ["Y haplogroups",printFrequencyMaybeString ", " $ frequency $ map posSamYHaplogroup xs],
                 ["% endogenous human DNA", meanAndSdRoundTo 2 $ map (\(Percent x) -> x) $ mapMaybe posSamEndogenous xs],
                 ["Nr of SNPs on 1240K", meanAndSdInteger $ map fromIntegral $ mapMaybe posSamNrAutosomalSNPs xs],
                 ["Coverage on 1240K", meanAndSdRoundTo 2 $ mapMaybe posSamCoverage1240K xs],
@@ -91,7 +91,6 @@ printFrequency sep (x:xs) = show (fst x) ++ ": " ++ show (snd x) ++ sep ++ print
 -- | A helper function to print the output of frequency over Maybe values nicely
 printFrequencyMaybe :: Show a => String -> [(Maybe a,Int)] -> String
 printFrequencyMaybe _ [] = "no values"
-printFrequencyMaybe _ [(Nothing,_)] = "no values"
 printFrequencyMaybe _ [x] = maybeShow (fst x) ++ ": " ++ show (snd x)
 printFrequencyMaybe sep (x:xs) = maybeShow (fst x) ++ ": " ++ show (snd x) ++ sep ++ printFrequencyMaybe sep xs
 
@@ -99,3 +98,20 @@ printFrequencyMaybe sep (x:xs) = maybeShow (fst x) ++ ": " ++ show (snd x) ++ se
 maybeShow :: Show a => Maybe a -> String
 maybeShow (Just x) = show x
 maybeShow Nothing  = "n/a"
+
+-- | As printFrequency, but without additional quoting of strings
+printFrequencyString :: String -> [(String,Int)] -> String
+printFrequencyString _ [] = "no values"
+printFrequencyString _ [x] = fst x ++ ": " ++ show (snd x)
+printFrequencyString sep (x:xs) = fst x ++ ": " ++ show (snd x) ++ sep ++ printFrequencyString sep xs
+
+-- | As printFrequencyMaybe, but without additional quoting of strings
+printFrequencyMaybeString :: String -> [(Maybe String,Int)] -> String
+printFrequencyMaybeString _ [] = "no values"
+printFrequencyMaybeString _ [x] = maybeShowString (fst x) ++ ": " ++ show (snd x)
+printFrequencyMaybeString sep (x:xs) = maybeShowString (fst x) ++ ": " ++ show (snd x) ++ sep ++ printFrequencyMaybeString sep xs
+
+-- | As maybeShow, but without additional quoting of strings
+maybeShowString :: Maybe String -> String
+maybeShowString (Just x) = x
+maybeShowString Nothing  = "n/a"
--- test/Poseidon/SummariseSpec.hs
+++ test/Poseidon/SummariseSpec.hs
@@ -27,7 +27,7 @@ testFrequency =
     describe "Poseidon.CLI.Summarise.frequency" $ do
     it "should calculate frequencies correctly for strings" $ do
         frequency ["ab", "bc", "cd", "cd", "ab"] `shouldBe` 
-            [("ab", 2), ("bc", 1), ("cd", 2)]
+            [("ab", 2), ("cd", 2), ("bc", 1)]
     it "should calculate frequencies correctly for integers" $ do
         frequency [1, 2, 3, 1, 1] `shouldBe` 
             [(1, 3), (2, 1), (3, 1)]
@@ -39,7 +39,7 @@ testPrintFrequency =
         printFrequency ":-)" ([] :: [(Int, Int)]) `shouldBe` "no values"
     it "should display frequencies correctly for strings" $ do
         printFrequency ", " (frequency ["ab", "bc", "cd", "cd", "ab"]) `shouldBe` 
-            "\"ab\": 2, \"bc\": 1, \"cd\": 2"
+            "\"ab\": 2, \"cd\": 2, \"bc\": 1"
     it "should display frequencies correctly for integers" $ do
         printFrequency " | " (frequency [1, 2, 3, 1, 1]) `shouldBe` 
             "1: 3 | 2: 1 | 3: 1"
@@ -50,10 +50,10 @@ testPrintFrequencyMaybe =
     it "should deal with an empty list correctly" $ do
         printFrequencyMaybe ":-)" ([] :: [(Maybe Int, Int)]) `shouldBe` "no values"
     it "should deal with an effectivly empty list correctly" $ do
-        printFrequencyMaybe ":-)" ([(Nothing, 3)] :: [(Maybe Int, Int)]) `shouldBe` "no values"
+        printFrequencyMaybe ":-)" ([(Nothing, 3)] :: [(Maybe Int, Int)]) `shouldBe` "n/a: 3"
     it "should display frequencies correctly for strings" $ do
         printFrequencyMaybe ", " (frequency [Just "ab", Just "bc", Nothing, Just "ab"]) `shouldBe` 
-            "n/a: 1, \"ab\": 2, \"bc\": 1"
+            "\"ab\": 2, n/a: 1, \"bc\": 1"
     it "should display frequencies correctly for integers" $ do
         printFrequencyMaybe " | " (frequency [Just 1, Just 2, Nothing, Just 1, Nothing]) `shouldBe` 
             "n/a: 2 | 1: 2 | 2: 1"
