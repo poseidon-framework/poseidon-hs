bcd7b53ba29095f06de6d0d7b1ac137935212be3
Stephan Schiffels <stephan_schiffels@mac.com>
2021-02-07 23:33:34 +0100

    completed automatic package zipping in server

--- src-executables/Main-server.hs
+++ src-executables/Main-server.hs
@@ -1,18 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import           Data.Aeson          (ToJSON, object, toJSON, (.=))
-import           Data.Text.Lazy      (pack)
-import           Data.Time           (Day)
-import           Data.Version        (Version, showVersion)
-import qualified Options.Applicative as OP
-import           Paths_poseidon_hs   (version)
-import           Poseidon.Package    (PoseidonPackage (..),
-                                      readPoseidonPackageCollection)
-import           Web.Scotty          (get, param, scotty, text, json)
+import           Poseidon.GenotypeData (GenotypeDataSpec (..))
+import           Poseidon.Package      (PoseidonPackage (..),
+                                        readPoseidonPackageCollection)
+
+import           Codec.Archive.Zip     (Archive, emptyArchive, fromArchive, toEntry, addEntryToArchive)
+import           Control.Monad         (forM)
+import           Data.Aeson            (ToJSON, object, toJSON, (.=))
+import qualified Data.ByteString.Lazy  as B
+import           Data.Text.Lazy        (pack, unpack)
+import           Data.Time             (Day)
+import           Data.Version          (Version, showVersion)
+import qualified Options.Applicative   as OP
+import           Paths_poseidon_hs     (version)
+import           System.FilePath.Posix ((<.>), (</>))
+import           System.IO             (hPutStrLn, stderr)
+import           System.Posix.Files    (getFileStatus, modificationTime)
+import           Web.Scotty            (file, get, json, notFound, param, raise,
+                                        scotty, text)
 
 data CommandLineOptions = CommandLineOptions
-    { cliBaseDirs :: [FilePath]
-    , cliPort     :: Int
+    { cliBaseDirs        :: [FilePath]
+    , cliPort            :: Int
+    , cliIgnoreGenoFiles :: Bool
     }
     deriving (Show)
 
@@ -41,19 +51,55 @@ packageToPackageInfo pac = PackageInfo {
 
 main :: IO ()
 main = do
-    opts@(CommandLineOptions baseDirs port) <- OP.customExecParser p optParserInfo
-    allPackages <- readPoseidonPackageCollection False True baseDirs
+    opts@(CommandLineOptions baseDirs port ignoreGenoFiles) <- OP.customExecParser p optParserInfo
+    allPackages <- readPoseidonPackageCollection False ignoreGenoFiles baseDirs
+    zipDict <- forM allPackages (\pac -> do
+        hPutStrLn stderr ("Preparing Zip Archive for package " ++ posPacTitle pac)
+        zip <- makeZipArchive pac ignoreGenoFiles
+        let zip_raw = fromArchive zip
+        let fn = posPacBaseDir pac <.> "zip"
+        B.writeFile fn zip_raw
+        return (posPacTitle pac, fn))
     scotty port $ do
         get "/packages" $
             (json . map packageToPackageInfo) allPackages
         get "/:package_name/zip_file" $ do
             p <- param "package_name"
-            text ("You requested the zip_file for package " <> p)
-        get "/:package_name/individuals" $ do
-            p <- param "package_name"
-            text ("You requested the individual entries for package " <> p)
+            let zipFN = lookup (unpack p) zipDict
+            case zipFN of
+                Just fn -> file fn
+                Nothing -> raise ("unknown package " <> p)
+        notFound $ raise "Unknown request"
   where
     p = OP.prefs OP.showHelpOnEmpty
+
+makeZipArchive :: PoseidonPackage -> Bool -> IO Archive
+makeZipArchive pac ignoreGenoFiles =
+    return emptyArchive >>= addYaml >>= addJanno >>= addBib >>= addInd >>= addSnp >>= addGeno
+  where
+    addYaml = addFN "POSEIDON.yml" (posPacBaseDir pac)
+    addJanno = case posPacJannoFile pac of
+        Nothing -> return
+        Just fn -> addFN fn (posPacBaseDir pac)
+    addBib = case posPacBibFile pac of
+        Nothing -> return
+        Just fn -> addFN fn (posPacBaseDir pac)
+    addInd = addFN (indFile . posPacGenotypeData $ pac) (posPacBaseDir pac)
+    addSnp = if ignoreGenoFiles
+             then return
+             else addFN (snpFile . posPacGenotypeData $ pac) (posPacBaseDir pac)
+    addGeno = if ignoreGenoFiles
+              then return
+              else addFN (genoFile . posPacGenotypeData $ pac) (posPacBaseDir pac)
+    addFN :: FilePath -> FilePath -> Archive -> IO Archive
+    addFN fn baseDir a = do
+        let fullFN = baseDir </> fn
+        raw <- B.readFile fullFN
+        fstatus <- getFileStatus fullFN
+        let modTime = (toInteger . fromEnum . modificationTime) fstatus
+        let zipEntry = toEntry fn modTime raw
+        return (addEntryToArchive zipEntry a)
+
 
 optParserInfo :: OP.ParserInfo CommandLineOptions
 optParserInfo = OP.info (OP.helper <*> versionOption <*> optParser) (
@@ -69,7 +115,7 @@ versionOption :: OP.Parser (a -> a)
 versionOption = OP.infoOption (showVersion version) (OP.long "version" <> OP.help "Show version")
 
 optParser :: OP.Parser CommandLineOptions
-optParser = CommandLineOptions <$> parseBasePaths <*> parsePort
+optParser = CommandLineOptions <$> parseBasePaths <*> parsePort <*> parseIgnoreGenoFiles
 
 parseBasePaths :: OP.Parser [FilePath]
 parseBasePaths = OP.some (OP.strOption (OP.long "baseDir" <>
@@ -81,3 +127,7 @@ parsePort :: OP.Parser Int
 parsePort = OP.option OP.auto (OP.long "port" <> OP.short 'p' <> OP.metavar "PORT" <>
     OP.value 3000 <> OP.showDefault <>
     OP.help "the port on which the server listens")
+
+parseIgnoreGenoFiles :: OP.Parser Bool
+parseIgnoreGenoFiles = OP.switch (OP.long "ignoreGenoFiles" <> OP.short 'i' <>
+    OP.help "whether to ignore the bed and SNP files. Useful for debugging")
