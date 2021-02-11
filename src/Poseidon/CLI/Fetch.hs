6428efbc1d2e8c9fe02d4d97347fc96ab04aefec
Clemens Schmid <clemens@nevrome.de>
2021-02-11 21:53:10 +0100

    small experiments with http-conduit

--- src/Poseidon/CLI/Fetch.hs
+++ src/Poseidon/CLI/Fetch.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Poseidon.CLI.Fetch where
 
 import           Poseidon.ForgeRecipe       (ForgeEntity (..), ForgeRecipe (..), 
                                              readEntitiesFromFile)
 
+import           Data.Aeson                 (Value)
+import           Network.HTTP.Simple        (getResponseStatusCode, 
+                                             httpJSON, 
+                                             Response (..))
 import           System.IO                  (hPutStrLn, stderr, hFlush, hPutStr)
+
 
 data FetchOptions = FetchOptions
     { _jaBaseDirs :: [FilePath]
@@ -16,6 +23,7 @@ data FetchOptions = FetchOptions
 -- | The main function running the Fetch command
 runFetch :: FetchOptions -> IO ()
 runFetch (FetchOptions baseDirs entitiesDirect entitiesFile) = do --onlyPreview remoteURL) = do
-    hPutStrLn stderr "4"
+    response <- httpJSON "http://c107-224.cloud.gwdg.de:3000/packages" :: IO (Response ())
+    putStrLn $ "The status code was: " ++ show (getResponseStatusCode response)
 
  
