b58f321027867d26e38531a04c90256daa931f80
Clemens Schmid <clemens@nevrome.de>
2021-03-07 14:52:50 +0100

    wrote general genotype data conversion code

--- src/Poseidon/CLI/Genoconvert.hs
+++ src/Poseidon/CLI/Genoconvert.hs
@@ -1,12 +1,24 @@
 module Poseidon.CLI.Genoconvert where
 
 import           Poseidon.GenotypeData      (GenotypeDataSpec (..),
-                                             GenotypeFormatSpec (..))
+                                             GenotypeFormatSpec (..),
+                                             loadGenotypeData)
 import           Poseidon.Package           (findAllPoseidonYmlFiles,
                                              readPoseidonPackageCollection,
                                              PoseidonPackage (..))
 
-import           System.IO                  (hPutStr, hPutStrLn, stderr)
+import           Control.Monad              (when)
+import           Pipes                      (MonadIO (liftIO), Pipe (..), await,
+                                             lift, runEffect, yield, (>->))
+import qualified Pipes.Prelude              as P
+import           Pipes.Safe                 (SafeT (..), runSafeT, throwM)
+import           SequenceFormats.Eigenstrat (EigenstratIndEntry (..),
+                                             EigenstratSnpEntry (..), GenoLine,
+                                             writeEigenstrat)
+import           SequenceFormats.Plink      (writePlink)
+import           System.Console.ANSI        (hClearLine, hSetCursorColumn)
+import           System.FilePath            ((<.>), (</>))
+import           System.IO                  (hFlush, hPutStr, hPutStrLn, stderr)
 
 -- | A datatype representing command line options for the validate command
 data GenoconvertOptions = GenoconvertOptions
@@ -22,10 +34,43 @@ runGenoconvert (GenoconvertOptions baseDirs outFormat) = do
     mapM_ (convertGenoTo outFormat) allPackages
 
 convertGenoTo :: GenotypeFormatSpec -> PoseidonPackage -> IO ()
-convertGenoTo genoFormat pac = do
-    hPutStr stderr $
+convertGenoTo outFormat pac = do
+    -- start message
+    hPutStrLn stderr $
         "Converting genotype data in package "
         ++ posPacTitle pac
         ++ " to format "
-        ++ show genoFormat
+        ++ show outFormat
+        ++ ":"
+    -- compile file names paths
+    let outName = posPacTitle pac
+    let [outInd, outSnp, outGeno] = case outFormat of 
+            GenotypeFormatEigenstrat -> [outName <.> ".ind", outName <.> ".snp", outName <.> ".geno"]
+            GenotypeFormatPlink -> [outName <.> ".fam", outName <.> ".bim", outName <.> ".bed"]
+    if format (posPacGenotypeData pac) /= outFormat
+    then do
+        runSafeT $ do
+            (eigenstratIndEntries, eigenstratProd) <- loadGenotypeData (posPacBaseDir pac) (posPacGenotypeData pac)
+            let [outG, outS, outI] = map (posPacBaseDir pac </>) [outGeno, outSnp, outInd]
+            let outConsumer = case outFormat of
+                    GenotypeFormatEigenstrat -> writeEigenstrat outG outS outI eigenstratIndEntries
+                    GenotypeFormatPlink -> writePlink outG outS outI eigenstratIndEntries
+            runEffect $ eigenstratProd >-> printProgress >-> outConsumer
+            liftIO $ hClearLine stderr
+            liftIO $ hSetCursorColumn stderr 0
+            liftIO $ hPutStrLn stderr "SNPs processed: All done"
+    else hPutStrLn stderr 
+        "The genotype data is already in the requested format"
         
+printProgress :: Pipe a a (SafeT IO) ()
+printProgress = loop 0
+  where
+    loop n = do
+        when (n `rem` 1000 == 0) $ do
+            liftIO $ hClearLine stderr
+            liftIO $ hSetCursorColumn stderr 0
+            liftIO $ hPutStr stderr ("SNPs processed: " ++ show n)
+            liftIO $ hFlush stderr
+        x <- await
+        yield x
+        loop (n+1)
