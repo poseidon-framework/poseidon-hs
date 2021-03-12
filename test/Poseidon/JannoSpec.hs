90c54c5fbe13a14160ca52ae08c4c3344748bb54
Stephan Schiffels <stephan_schiffels@mac.com>
2021-03-12 19:48:47 +0100

    fixed tests

--- test/Poseidon/JannoSpec.hs
+++ test/Poseidon/JannoSpec.hs
@@ -1,6 +1,6 @@
 module Poseidon.JannoSpec (spec) where
 
-import           Poseidon.Janno            (PoseidonSample (..),
+import           Poseidon.Janno            (JannoRow (..),
                                             Sex (..),
                                             Latitude (..),
                                             Longitude (..),
@@ -34,44 +34,44 @@ testPoseidonSampleFromJannoFile = describe "Poseidon.Janno.readJannoFile" $ do
         janno_partial <- readJannoFile minimalPartialJannoPath
         janno `shouldBe` janno_partial
         length janno `shouldBe` 3
-        map posSamIndividualID janno   `shouldBe` ["XXX011", "XXX012", "XXX013"]
-        map posSamCollectionID janno   `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamSourceTissue janno   `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamLatitude janno       `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamLongitude janno      `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamDateC14UncalBP janno `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamDateBCADMedian janno `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamDateType janno       `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamDataType janno       `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamGenotypePloidy janno `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamGroupName janno      `shouldBe` [["POP1"], ["POP2"], ["POP1"]]
-        map posSamGeneticSex janno     `shouldBe` [Male, Female, Male]
-        map posSamCoverage1240K janno  `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamUDG janno            `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamLibraryBuilt janno   `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamDamage janno         `shouldBe` [Nothing, Nothing, Nothing]
+        map jIndividualID janno   `shouldBe` ["XXX011", "XXX012", "XXX013"]
+        map jCollectionID janno   `shouldBe` [Nothing, Nothing, Nothing]
+        map jSourceTissue janno   `shouldBe` [Nothing, Nothing, Nothing]
+        map jLatitude janno       `shouldBe` [Nothing, Nothing, Nothing]
+        map jLongitude janno      `shouldBe` [Nothing, Nothing, Nothing]
+        map jDateC14UncalBP janno `shouldBe` [Nothing, Nothing, Nothing]
+        map jDateBCADMedian janno `shouldBe` [Nothing, Nothing, Nothing]
+        map jDateType janno       `shouldBe` [Nothing, Nothing, Nothing]
+        map jDataType janno       `shouldBe` [Nothing, Nothing, Nothing]
+        map jGenotypePloidy janno `shouldBe` [Nothing, Nothing, Nothing]
+        map jGroupName janno      `shouldBe` [["POP1"], ["POP2"], ["POP1"]]
+        map jGeneticSex janno     `shouldBe` [Male, Female, Male]
+        map jCoverage1240K janno  `shouldBe` [Nothing, Nothing, Nothing]
+        map jUDG janno            `shouldBe` [Nothing, Nothing, Nothing]
+        map jLibraryBuilt janno   `shouldBe` [Nothing, Nothing, Nothing]
+        map jDamage janno         `shouldBe` [Nothing, Nothing, Nothing]
     it "should read normal janno files correctly" $ do
         janno <- readJannoFile normalFullJannoPath
         janno_partial <- readJannoFile normalPartialJannoPath
         janno `shouldBe` janno_partial
         length janno `shouldBe` 3
-        map posSamIndividualID janno   `shouldBe` ["XXX011", "XXX012", "XXX013"]
-        map posSamCollectionID janno   `shouldBe` [Nothing, Nothing, Nothing]
-        map posSamSourceTissue janno   `shouldBe` [Just ["xxx", "yyy"], Just ["xxx"], Just ["xxx"]]
-        map posSamCountry janno        `shouldBe` [Just "xxx", Just "xxx", Just "xxx"]
-        map posSamLatitude janno       `shouldBe` [Just (Latitude 0), Just (Latitude (-90)), Just (Latitude 90)]
-        map posSamLongitude janno      `shouldBe` [Just (Longitude 0), Just (Longitude (-180)), Just (Longitude 180)]
-        map posSamDateC14UncalBP janno `shouldBe` [Just [3000, 3100, 2900], Nothing, Nothing]
-        map posSamDateBCADMedian janno `shouldBe` [Just (-1000), Just (-5000), Just 2000]
-        map posSamDateType janno       `shouldBe` [Just C14, Just Contextual, Just Modern]
-        map posSamDataType janno       `shouldBe` [Just [Shotgun, A1240K], Just [A1240K], Just [ReferenceGenome]]
-        map posSamGenotypePloidy janno `shouldBe` [Just Diploid, Just Haploid, Just Diploid]
-        map posSamGroupName janno      `shouldBe` [["POP1", "POP3"], ["POP2"], ["POP1"]]
-        map posSamGeneticSex janno     `shouldBe` [Male, Female, Male]
-        map posSamCoverage1240K janno  `shouldBe` [Just 0, Just 0, Just 0]
-        map posSamUDG janno            `shouldBe` [Just Minus, Just Half, Just Plus]
-        map posSamLibraryBuilt janno   `shouldBe` [Just DS, Just SS, Just Other]
-        map posSamDamage janno         `shouldBe` [Just (Percent 0), Just (Percent 100), Just (Percent 50)]
+        map jIndividualID janno   `shouldBe` ["XXX011", "XXX012", "XXX013"]
+        map jCollectionID janno   `shouldBe` [Nothing, Nothing, Nothing]
+        map jSourceTissue janno   `shouldBe` [Just ["xxx", "yyy"], Just ["xxx"], Just ["xxx"]]
+        map jCountry janno        `shouldBe` [Just "xxx", Just "xxx", Just "xxx"]
+        map jLatitude janno       `shouldBe` [Just (Latitude 0), Just (Latitude (-90)), Just (Latitude 90)]
+        map jLongitude janno      `shouldBe` [Just (Longitude 0), Just (Longitude (-180)), Just (Longitude 180)]
+        map jDateC14UncalBP janno `shouldBe` [Just [3000, 3100, 2900], Nothing, Nothing]
+        map jDateBCADMedian janno `shouldBe` [Just (-1000), Just (-5000), Just 2000]
+        map jDateType janno       `shouldBe` [Just C14, Just Contextual, Just Modern]
+        map jDataType janno       `shouldBe` [Just [Shotgun, A1240K], Just [A1240K], Just [ReferenceGenome]]
+        map jGenotypePloidy janno `shouldBe` [Just Diploid, Just Haploid, Just Diploid]
+        map jGroupName janno      `shouldBe` [["POP1", "POP3"], ["POP2"], ["POP1"]]
+        map jGeneticSex janno     `shouldBe` [Male, Female, Male]
+        map jCoverage1240K janno  `shouldBe` [Just 0, Just 0, Just 0]
+        map jUDG janno            `shouldBe` [Just Minus, Just Half, Just Plus]
+        map jLibraryBuilt janno   `shouldBe` [Just DS, Just SS, Just Other]
+        map jDamage janno         `shouldBe` [Just (Percent 0), Just (Percent 100), Just (Percent 50)]
     it "should fail to read borked janno files" $ do
         readJannoFile borkedFullJannoPath `shouldThrow` anyException
         readJannoFile borkedPartialJannoPath `shouldThrow` anyException
