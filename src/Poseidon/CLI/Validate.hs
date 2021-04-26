2ca62f9402466778438fcd010d90dacb69be832c
Stephan Schiffels <stephan_schiffels@mac.com>
2021-04-26 15:06:22 +0200

    added simple 100-line running of genotype data. Compiles, but not tested yet

--- src/Poseidon/CLI/Validate.hs
+++ src/Poseidon/CLI/Validate.hs
@@ -2,18 +2,22 @@
 
 module Poseidon.CLI.Validate where
 
-import           Poseidon.Package           (findAllPoseidonYmlFiles,
-                                             readPoseidonPackageCollection,
-                                             PoseidonPackage (..))
+import           Poseidon.Package (PoseidonPackage (..),
+                                   findAllPoseidonYmlFiles,
+                                   getJointGenotypeData,
+                                   readPoseidonPackageCollection)
 
-import           Control.Monad              (unless)
-import           Data.List                  (foldl')
-import           System.Exit                (exitFailure, exitSuccess)
-import           System.IO                  (hPutStrLn, stderr)
+import           Control.Monad    (unless)
+import           Data.List        (foldl')
+import           Pipes            (runEffect, (>->))
+import qualified Pipes.Prelude    as P
+import           Pipes.Safe       (runSafeT)
+import           System.Exit      (exitFailure, exitSuccess)
+import           System.IO        (hPutStrLn, stderr)
 
 -- | A datatype representing command line options for the validate command
 data ValidateOptions = ValidateOptions
-    { _jaBaseDirs :: [FilePath]
+    { _jaBaseDirs    :: [FilePath]
     , _optIgnoreGeno :: Bool
     }
 
@@ -23,10 +27,13 @@ runValidate (ValidateOptions baseDirs ignoreGeno) = do
     allPackages <- readPoseidonPackageCollection True False ignoreGeno baseDirs
     let numberOfPOSEIDONymlFiles = length posFiles
         numberOfLoadedPackagesWithDuplicates = foldl' (+) 0 $ map posPacDuplicate allPackages
+    runSafeT $ do
+        (_, eigenstratProd) <- getJointGenotypeData False False allPackages
+        runEffect $ eigenstratProd >-> P.take 100 >-> P.drain
     if numberOfPOSEIDONymlFiles == numberOfLoadedPackagesWithDuplicates
     then do
         hPutStrLn stderr "Validation passed ✓"
-        exitSuccess 
+        exitSuccess
     else do
         hPutStrLn stderr "Validation failed ✗"
         exitFailure
