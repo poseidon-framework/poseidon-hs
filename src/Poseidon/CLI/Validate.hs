339235a740fe22c4c68af0a1b19c0b690595d73a
Clemens Schmid <clemens@nevrome.de>
2021-04-27 23:16:11 +0200

    more correct implementation of the joint genotype check in validate to make sure that validate always ends with *passed* or *failed*

--- src/Poseidon/CLI/Validate.hs
+++ src/Poseidon/CLI/Validate.hs
@@ -2,18 +2,20 @@
 
 module Poseidon.CLI.Validate where
 
-import           Poseidon.Package (PoseidonPackage (..),
+import           Poseidon.Package  (PoseidonPackage (..),
                                    findAllPoseidonYmlFiles,
                                    getJointGenotypeData,
                                    readPoseidonPackageCollection)
 
-import           Control.Monad    (unless)
-import           Data.List        (foldl')
-import           Pipes            (runEffect, (>->))
-import qualified Pipes.Prelude    as P
-import           Pipes.Safe       (runSafeT)
-import           System.Exit      (exitFailure, exitSuccess)
-import           System.IO        (hPutStrLn, stderr)
+import           Control.Exception (SomeException, catch)
+import           Control.Monad     (unless)
+import           Data.List         (foldl')
+import           Pipes             (runEffect, (>->))
+import qualified Pipes.Prelude     as P
+import           Pipes.Safe        (runSafeT)
+import           System.Exit       (exitFailure, exitSuccess)
+import           System.IO         (hPutStrLn, stderr)
+
 
 -- | A datatype representing command line options for the validate command
 data ValidateOptions = ValidateOptions
@@ -27,14 +29,32 @@ runValidate (ValidateOptions baseDirs ignoreGeno) = do
     allPackages <- readPoseidonPackageCollection True False ignoreGeno True baseDirs
     let numberOfPOSEIDONymlFiles = length posFiles
         numberOfLoadedPackagesWithDuplicates = foldl' (+) 0 $ map posPacDuplicate allPackages
-    if numberOfPOSEIDONymlFiles == numberOfLoadedPackagesWithDuplicates
+    check <- if numberOfPOSEIDONymlFiles == numberOfLoadedPackagesWithDuplicates
+             then if ignoreGeno
+                  then return True
+                  else do checkJointGenotypeData allPackages
+             else return False
+    if check
     then do
-        hPutStrLn stderr "Checking joint genotype data for consistency"
-        runSafeT $ do
-            (_, eigenstratProd) <- getJointGenotypeData False False allPackages
-            runEffect $ eigenstratProd >-> P.take 100 >-> P.drain
         hPutStrLn stderr "Validation passed ✓"
         exitSuccess
     else do
         hPutStrLn stderr "Validation failed ✗"
         exitFailure
+
+checkJointGenotypeData :: [PoseidonPackage] -> IO Bool
+checkJointGenotypeData packageList = do
+    hPutStrLn stderr "Checking joint genotype data for consistency..."
+    parseFirst100SNPs packageList `catch` handler
+    where
+        handler :: SomeException -> IO Bool
+        handler e = do
+            print e
+            return False
+
+parseFirst100SNPs :: [PoseidonPackage] -> IO Bool
+parseFirst100SNPs packageList = do
+    runSafeT $ do
+        (_, eigenstratProd) <- getJointGenotypeData False False packageList
+        runEffect $ eigenstratProd >-> P.take 100 >-> P.drain
+    return True
