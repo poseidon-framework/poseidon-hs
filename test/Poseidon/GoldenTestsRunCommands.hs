52459e7fe4576a427b977b99f254ee20c9f01681
Clemens Schmid <clemens@nevrome.de>
2021-05-28 17:32:25 +0200

    refactoring in the golden test module

--- test/Poseidon/GoldenTestsRunCommands.hs
+++ test/Poseidon/GoldenTestsRunCommands.hs
@@ -12,8 +12,8 @@ import           System.Directory           (createDirectory, removeDirectoryRec
 import           System.FilePath.Posix      ((</>))
 import           System.IO                  (stdout, IOMode(WriteMode), withFile, openFile, stderr)
 
-temporaryTestDir :: FilePath
-temporaryTestDir = "/tmp/trident_CLI_test"
+tempTestDir :: FilePath
+tempTestDir = "/tmp/trident_CLI_test"
 
 staticCheckSumFile :: FilePath
 staticCheckSumFile = "test/testDat/staticCheckSumFile.txt"
@@ -29,7 +29,7 @@ createDynamicCheckSumFile = runCLICommands dynamicCheckSumFile
 
 fetchOpts1 :: FetchOptions
 fetchOpts1 = FetchOptions
-    { _jaBaseDirs       = [temporaryTestDir]
+    { _jaBaseDirs       = [tempTestDir]
     , _entityList       = [Pac "2019_Nikitin_LBK"]
     , _entityFiles      = []
     , _remoteURL        = "https://c107-224.cloud.gwdg.de"
@@ -39,37 +39,48 @@ fetchOpts1 = FetchOptions
 
 listOpts1 :: ListOptions
 listOpts1 = ListOptions
-    { _loRepoLocation  = RepoLocal [temporaryTestDir  </> "2019_Nikitin_LBK"]
+    { _loRepoLocation  = RepoLocal [tempTestDir  </> "2019_Nikitin_LBK"]
     , _loListEntity    = ListPackages
     , _loRawOutput     = False
     , _optIgnoreGeno   = False
     }
 
+listOpts2 :: ListOptions
+listOpts2 = ListOptions
+    { _loRepoLocation  = RepoLocal [tempTestDir  </> "2019_Nikitin_LBK"]
+    , _loListEntity    = ListGroups
+    , _loRawOutput     = False
+    , _optIgnoreGeno   = False
+    }
+
 runCLICommands :: FilePath -> IO ()
-runCLICommands checkSumFilePath = do
+runCLICommands checkFilePath = do
     -- create temp dir for test output
-    testDirExists <- doesDirectoryExist temporaryTestDir
+    testDirExists <- doesDirectoryExist tempTestDir
     if testDirExists
     then do 
-        removeDirectoryRecursive temporaryTestDir
-        createDirectory temporaryTestDir
-    else createDirectory temporaryTestDir
+        removeDirectoryRecursive tempTestDir
+        createDirectory tempTestDir
+    else createDirectory tempTestDir
     -- create error sink
     devNull <- openFile "/dev/null" WriteMode
     hDuplicateTo devNull stderr
     -- fetch
     runFetch fetchOpts1
-    fetchCheck1 <- getChecksum $ temporaryTestDir </> "2019_Nikitin_LBK" </> "POSEIDON.yml"
-    writeFile checkSumFilePath $ fetchCheck1 ++ " fetchCheck1 POSEIDON.yml"
+    fetchCheck1 <- getChecksum $ tempTestDir </> "2019_Nikitin_LBK" </> "POSEIDON.yml"
+    writeFile checkFilePath $ fetchCheck1 ++ " fetchCheck1 POSEIDON.yml"
     -- list
-    withFile (temporaryTestDir </> "listStdOut.txt") WriteMode $ \handle -> do
-        stdout_old <- hDuplicate stdout -- backup stdout
-        hDuplicateTo handle stdout -- redirect stdout to file
-        runList listOpts1
-        hDuplicateTo stdout_old stdout -- load backup again
-    listCheck1 <- getChecksum $ temporaryTestDir </> "listStdOut.txt"
-    appendFile checkSumFilePath $ "\n" ++ listCheck1 ++ " listCheck1 listStdOut.txt"
+    runAndStoreChecksum checkFilePath tempTestDir "tridentList1" (runList listOpts1)
+    runAndStoreChecksum checkFilePath tempTestDir "tridentList2" (runList listOpts2)
     -- close error sink
     hClose devNull
-    -- delete temp dir for test output
-    -- removeDirectoryRecursive temporaryTestDir
+
+runAndStoreChecksum :: FilePath -> FilePath -> String -> IO () -> IO ()
+runAndStoreChecksum checkSumFilePath testDir outFileName action = do
+    withFile (testDir </> outFileName) WriteMode $ \handle -> do
+        stdout_old <- hDuplicate stdout -- backup stdout
+        hDuplicateTo handle stdout -- redirect stdout to file
+        action -- run action
+        hDuplicateTo stdout_old stdout -- load backup again
+    listCheck1 <- getChecksum $ testDir </> outFileName
+    appendFile checkSumFilePath $ "\n" ++ listCheck1 ++ " " ++ outFileName
