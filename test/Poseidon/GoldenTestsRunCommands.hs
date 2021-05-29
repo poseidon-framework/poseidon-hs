15e4ee105091f55a5dccf300c2af82c9c4876bfb
Clemens Schmid <clemens@nevrome.de>
2021-05-29 16:04:03 +0200

    decided to include the test CLI pipeline's output files in the repo to find deviations more easily

--- test/Poseidon/GoldenTestsRunCommands.hs
+++ test/Poseidon/GoldenTestsRunCommands.hs
@@ -8,13 +8,17 @@ import           Poseidon.CLI.List          (ListOptions (..), runList,
                                              RepoLocationSpec (..), ListEntity (..))
 import           Poseidon.Package           (getChecksum)
 
+import           Control.Monad              (when)
 import           GHC.IO.Handle              (hDuplicateTo, hDuplicate, hClose)
-import           System.Directory           (createDirectory, removeDirectoryRecursive, doesDirectoryExist)
+import           System.Directory           (createDirectory, removeDirectoryRecursive, doesDirectoryExist, copyFile)
 import           System.FilePath.Posix      ((</>))
 import           System.IO                  (stdout, IOMode(WriteMode), withFile, openFile, stderr)
 
 tempTestDir :: FilePath
-tempTestDir = "/tmp/trident_CLI_test"
+tempTestDir = "/tmp/poseidonHSGoldenTestData"
+
+staticTestDir :: FilePath
+staticTestDir = "test/testDat/poseidonHSGoldenTestData"
 
 staticCheckSumFile :: FilePath
 staticCheckSumFile = "test/testDat/staticCheckSumFile.txt"
@@ -23,28 +27,12 @@ dynamicCheckSumFile :: FilePath
 dynamicCheckSumFile = "/tmp/poseidon_trident_dynamicCheckSumFile.txt"
 
 createStaticCheckSumFile :: FilePath -> IO ()
-createStaticCheckSumFile poseidonHSDir = runCLICommands $ poseidonHSDir </> staticCheckSumFile 
+createStaticCheckSumFile poseidonHSDir = runCLICommands 
+    (poseidonHSDir </> staticTestDir) 
+    (poseidonHSDir </> staticCheckSumFile )
 
 createDynamicCheckSumFile :: IO ()
-createDynamicCheckSumFile = runCLICommands dynamicCheckSumFile 
-
-fetchOpts1 :: FetchOptions
-fetchOpts1 = FetchOptions
-    { _jaBaseDirs       = [tempTestDir]
-    , _entityList       = [Pac "2019_Nikitin_LBK"]
-    , _entityFiles      = []
-    , _remoteURL        = "https://c107-224.cloud.gwdg.de"
-    , _upgrade          = True
-    , _downloadAllPacs  = False 
-    }
-
-listOpts1 :: ListOptions
-listOpts1 = ListOptions
-    { _loRepoLocation  = RepoLocal [tempTestDir  </> "2019_Nikitin_LBK"]
-    , _loListEntity    = ListPackages
-    , _loRawOutput     = False
-    , _optIgnoreGeno   = False
-    }
+createDynamicCheckSumFile = runCLICommands tempTestDir dynamicCheckSumFile 
 
 listOpts2 :: ListOptions
 listOpts2 = ListOptions
@@ -54,33 +42,50 @@ listOpts2 = ListOptions
     , _optIgnoreGeno   = False
     }
 
-runCLICommands :: FilePath -> IO ()
-runCLICommands checkFilePath = do
+runCLICommands :: FilePath -> FilePath -> IO ()
+runCLICommands testDir checkFilePath = do
     -- create temp dir for test output
-    testDirExists <- doesDirectoryExist tempTestDir
-    if testDirExists
-    then do 
-        removeDirectoryRecursive tempTestDir
-        createDirectory tempTestDir
-    else createDirectory tempTestDir
+    tmpTestDirExists <- doesDirectoryExist testDir
+    when tmpTestDirExists $ removeDirectoryRecursive testDir
+    createDirectory testDir
     -- create/overwrite checksum file
     writeFile checkFilePath "Checksums for trident CLI output \n\ 
         \Automatically generated with: ... \n\
         \"
     -- create error sink
     devNull <- openFile "/dev/null" WriteMode
+    stderr_old <- hDuplicate stderr
     hDuplicateTo devNull stderr
     -- fetch
-    runAndChecksumFiles checkFilePath tempTestDir (runFetch fetchOpts1) [
+    let fetchOpts1 = FetchOptions { 
+          _jaBaseDirs       = [testDir]
+        , _entityList       = [Pac "2019_Nikitin_LBK"]
+        , _entityFiles      = []
+        , _remoteURL        = "https://c107-224.cloud.gwdg.de"
+        , _upgrade          = True
+        , _downloadAllPacs  = False 
+        }
+    runAndChecksumFiles checkFilePath testDir (runFetch fetchOpts1) [
           "2019_Nikitin_LBK" </> "POSEIDON.yml"
         , "2019_Nikitin_LBK" </> "Nikitin_LBK.janno"
         , "2019_Nikitin_LBK" </> "Nikitin_LBK.fam"
         ]
     -- list
-    runAndChecksumStdOut checkFilePath tempTestDir (runList listOpts1) "tridentList1"
-    runAndChecksumStdOut checkFilePath tempTestDir (runList listOpts2) "tridentList2"
+    let listOpts1 = ListOptions { 
+          _loRepoLocation  = RepoLocal [testDir  </> "2019_Nikitin_LBK"]
+        , _loListEntity    = ListPackages
+        , _loRawOutput     = False
+        , _optIgnoreGeno   = False
+        }
+    runAndChecksumStdOut checkFilePath testDir (runList listOpts1) "tridentList1"
+    let listOpts1 = listOpts1 { 
+          _loListEntity    = ListGroups
+        }
+    runAndChecksumStdOut checkFilePath testDir (runList listOpts2) "tridentList2"
     -- close error sink
     hClose devNull
+    hDuplicateTo stderr_old stderr
+
 
 runAndChecksumFiles :: FilePath -> FilePath -> IO () -> [FilePath] -> IO ()
 runAndChecksumFiles checkSumFilePath testDir action outFiles = do
