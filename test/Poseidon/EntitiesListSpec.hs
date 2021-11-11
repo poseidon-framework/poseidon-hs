3590f36fc2564eb041bf301c9e6c7a6837ab967d
Clemens Schmid <clemens@nevrome.de>
2021-11-11 14:33:30 +0100

    more tests for the entity list parsing

--- test/Poseidon/EntitiesListSpec.hs
+++ test/Poseidon/EntitiesListSpec.hs
@@ -3,7 +3,7 @@ module Poseidon.EntitiesListSpec (spec) where
 import           Poseidon.EntitiesList
 
 import           Test.Hspec
-import           Data.Either (fromRight)
+import           Data.Either (fromRight, isLeft)
 
 spec :: Spec
 spec = do
@@ -19,5 +19,15 @@ testReadPoseidonEntitiesString =
     it "should parse longer entity lists correctly" $ do
         fromRight [] (readPoseidonEntitiesString "<a>,b,*c*") `shouldBe` [Ind "a", Group "b", Pac "c"]
         fromRight [] (readPoseidonEntitiesString "<a1>,b1,<a2>,*c*,b2") `shouldBe` [Ind "a1", Group "b1", Ind "a2", Pac "c", Group "b2"]
+    it "should ignore spaces after commas" $ do
+        fromRight [] (readPoseidonEntitiesString "<a>, b, *c*") `shouldBe` [Ind "a", Group "b", Pac "c"]
+        fromRight [] (readPoseidonEntitiesString "*c*, b") `shouldBe` [Pac "c", Group "b"]
+    it "should fail with any other spaces" $ do
+        readPoseidonEntitiesString "<a> ,b,*c*"  `shouldSatisfy` isLeft
+        readPoseidonEntitiesString " <a>,b,*c*"  `shouldSatisfy` isLeft
+        readPoseidonEntitiesString "<a >,b,*c*"  `shouldSatisfy` isLeft
+        readPoseidonEntitiesString "< a>,b,*c*"  `shouldSatisfy` isLeft
+        readPoseidonEntitiesString "<a>,b,*c* "  `shouldSatisfy` isLeft
+        readPoseidonEntitiesString "<a>,b d,*c*" `shouldSatisfy` isLeft
 
 
