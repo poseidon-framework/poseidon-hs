fa89f2a892a5e2328ad90a771f604943cddb4da0
Clemens Schmid <clemens@nevrome.de>
2023-06-13 14:43:35 +0200

    stylish-haskell

--- src/Poseidon/CLI/Timetravel.hs
+++ src/Poseidon/CLI/Timetravel.hs
@@ -1,18 +1,21 @@
 module Poseidon.CLI.Timetravel where
 
-import           Poseidon.Utils   (PoseidonIO, logInfo)
-import           Poseidon.Package (PackageReadOptions (..),
-                                   defaultPackageReadOptions,
-                                   readPoseidonPackageCollection)
-import Poseidon.Chronicle (readChronicle, PoseidonPackageChronicle (..), chroniclePackages, PackageIteration (..))
+import           Poseidon.Chronicle     (PackageIteration (..),
+                                         PoseidonPackageChronicle (..),
+                                         chroniclePackages, readChronicle)
+import           Poseidon.Package       (PackageReadOptions (..),
+                                         defaultPackageReadOptions,
+                                         readPoseidonPackageCollection)
+import           Poseidon.Utils         (PoseidonIO, logInfo)
 
-import qualified Data.Set as S
-import System.FilePath (takeDirectory, (</>))
-import System.Process (callCommand)
-import System.Directory (createDirectoryIfMissing, doesFileExist, copyFile, listDirectory)
-import Control.Monad (forM_, filterM)
-import           Control.Monad.IO.Class  (liftIO)
-import Data.Version (showVersion)
+import           Control.Monad          (filterM, forM_)
+import           Control.Monad.IO.Class (liftIO)
+import qualified Data.Set               as S
+import           Data.Version           (showVersion)
+import           System.Directory       (copyFile, createDirectoryIfMissing,
+                                         doesFileExist, listDirectory)
+import           System.FilePath        (takeDirectory, (</>))
+import           System.Process         (callCommand)
 
 data TimetravelOptions = TimetravelOptions
     { _timetravelBaseDirs      :: [FilePath]
@@ -30,7 +33,7 @@ pacReadOpts = defaultPackageReadOptions {
 
 runTimetravel :: TimetravelOptions -> PoseidonIO ()
 runTimetravel (TimetravelOptions baseDirs chroniclePath) = do
-    
+
     allPackages <- readPoseidonPackageCollection pacReadOpts baseDirs
     pacsInBaseDirs <- chroniclePackages True allPackages
 
@@ -44,20 +47,21 @@ runTimetravel (TimetravelOptions baseDirs chroniclePath) = do
 
 recoverPacIter :: FilePath -> FilePath -> PackageIteration -> PoseidonIO ()
 recoverPacIter sourceDir destDir (PackageIteration title version commit path) = do
-    let pacIterName = title ++ "-" ++ showVersion version 
+    let pacIterName = title ++ "-" ++ showVersion version
     logInfo $ "Recovering package " ++ pacIterName
-    
+
     logInfo $ "Checking out commit " ++ commit ++ " in " ++ sourceDir
     liftIO $ callCommand ("git -C " ++ sourceDir ++ " checkout " ++ commit ++ " --quiet")
     -- Instead of this nasty system call we could do something like this:
     -- https://hackage.haskell.org/package/git-0.3.0/docs/Data-Git-Monad.html#v:withCommit
     -- Unfortunately this library is not maintained any more.
-    
+
     logInfo $ "Copying dir " ++ path ++ " to " ++ destDir
     liftIO $ copyDirectory (sourceDir </> path) (destDir </> pacIterName)
-    
+
     logInfo $ "Checking out master branch " ++ commit ++ " in " ++ sourceDir
     liftIO $ callCommand ("git -C " ++ sourceDir ++ " checkout master --quiet")
+    -- master must of course be changed - it should instead go back to the initial revision
 
 copyDirectory :: FilePath -> FilePath -> IO ()
 copyDirectory srcDir destDir = do
