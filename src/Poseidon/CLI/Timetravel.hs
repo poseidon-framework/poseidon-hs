fce89f1f3e85449e44e384999b7a30d1d66cfad4
Clemens Schmid <clemens@nevrome.de>
2023-06-13 10:36:47 +0200

    started to accumulate the necessary components for timetravel

--- src/Poseidon/CLI/Timetravel.hs
+++ src/Poseidon/CLI/Timetravel.hs
@@ -1,11 +1,14 @@
 module Poseidon.CLI.Timetravel where
 
-import           Poseidon.Package (PackageReadOptions (..),
-                                   defaultPackageReadOptions)
 import           Poseidon.Utils   (PoseidonIO, logInfo)
+import           Poseidon.Package (PackageReadOptions (..),
+                                   defaultPackageReadOptions,
+                                   readPoseidonPackageCollection)
+import Poseidon.Chronicle (readChronicle)
 
 data TimetravelOptions = TimetravelOptions
-    { _timetravelBaseDirs  :: [FilePath]
+    { _timetravelBaseDirs      :: [FilePath]
+    , _timetravelChronicleFile :: FilePath
     }
 
 pacReadOpts :: PackageReadOptions
@@ -18,8 +21,15 @@ pacReadOpts = defaultPackageReadOptions {
     }
 
 runTimetravel :: TimetravelOptions -> PoseidonIO ()
-runTimetravel (TimetravelOptions baseDirs) = do
-    logInfo $ show baseDirs
+runTimetravel (TimetravelOptions baseDirs chroniclePath) = do
+    
+    allPackages <- readPoseidonPackageCollection pacReadOpts baseDirs
+    chronicle <- readChronicle chroniclePath
+
+    logInfo "done"
+
+
+
     -- That would be exactly the logic we need:
     -- https://hackage.haskell.org/package/git-0.3.0/docs/Data-Git-Monad.html#v:withCommit
     -- Unfortunately the git library is not maintained any more.
