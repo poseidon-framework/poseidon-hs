6db282c4c8650c91f1f7d1b05906facc3c5fefa5
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2023-11-15 18:30:47 +0100

    first simple implementation, no compile, no test yet

--- src/Poseidon/CLI/Jannocoalesce.hs
+++ src/Poseidon/CLI/Jannocoalesce.hs
@@ -9,13 +9,24 @@ data JannoCoalesceOptions = JannoCoalesceOptions
     }
 
 runJannocoalesce :: JannoCoalesceOptions -> PoseidonIO ()
-runJannocoalesce (JannoCoalesceOptions source target outSpec fillColumns overwrite) = do
-    sourceJanno <- readJannoFile source
-    targetJanno <- readJannoFile target
-    newJanno = forM sourceJanno $ \sourceRow -> do
-        targetRow <- lookupRowById targetJanno sourceRow
+runJannocoalesce (JannoCoalesceOptions source target outSpec fields overwrite) = do
+    JannoRows sourceRows <- readJannoFile source
+    JannoRows targetRows <- readJannoFile target
+    newJanno <- forM targetRows $ \targetRow -> do
+        let posId = jPoseidonID targetRow
+            sourceRowCandidates = filter (\r -> jPoseidonId r == posId) sourceRows
+        case sourceRowCandidates of
+            [] -> return targetRow
+            [keyRow] -> return $ mergeRows targetRow keyRow fields overwrite
+            _ -> throwM $ PoseidonGenericException $ "source file contains multiple rows with key " ++ posId
+    let outPath = maybe target id outSpec 
+    liftIO $ writeJannoFile outPath newJanno
 
 
-lookupRowById :: (MonadThrow m) => JannoRows -> JannoRow -> m JannoRow
-lookupRowById = do
-    undefined
+mergeRow :: JannoRow -> JannoRow -> [String] -> Bool -> JannoRow
+mergeRow targetRow sourceRow fields overwrite = do
+    let targetRowRecord = toNamedRecord targetRow
+        sourceRowRecord = toNamedRecord sourceRow
+    fromNamedRecord $ HM.unionWith mergeIfMissing targetRowRecord sourceRowRecord
+  where
+    mergeIfMissing targetVal sourceVal = if targetVal `elem` ["n/a", ""] || overwrite then sourceVal else targetVal
