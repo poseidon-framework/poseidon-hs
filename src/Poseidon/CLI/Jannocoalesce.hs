cdaa4e5059a185f869dd0c33219e55a28cab2757
Stephan Schiffels <stephan_schiffels@eva.mpg.de>
2023-11-21 20:56:07 +0100

    continued

--- src/Poseidon/CLI/Jannocoalesce.hs
+++ src/Poseidon/CLI/Jannocoalesce.hs
@@ -1,4 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Poseidon.CLI.Jannocoalesce where
+
+import Poseidon.Janno (JannoRow(..), JannoRows(..), writeJannoFile, readJannoFile)
+import Poseidon.Utils (PoseidonIO, PoseidonException(..))
+
+import Control.Monad (forM)
+import Control.Monad.Catch (throwM, MonadThrow)
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Char8      as BSC
+import qualified Data.Csv as Csv
+import qualified Data.HashMap.Strict as HM
 
 data JannoCoalesceOptions = JannoCoalesceOptions
     { _jannocoalesceSource           :: FilePath
@@ -14,19 +25,26 @@ runJannocoalesce (JannoCoalesceOptions source target outSpec fields overwrite) =
     JannoRows targetRows <- readJannoFile target
     newJanno <- forM targetRows $ \targetRow -> do
         let posId = jPoseidonID targetRow
-            sourceRowCandidates = filter (\r -> jPoseidonId r == posId) sourceRows
+            sourceRowCandidates = filter (\r -> jPoseidonID r == posId) sourceRows
         case sourceRowCandidates of
             [] -> return targetRow
-            [keyRow] -> return $ mergeRows targetRow keyRow fields overwrite
+            [keyRow] -> mergeRow targetRow keyRow fields overwrite
             _ -> throwM $ PoseidonGenericException $ "source file contains multiple rows with key " ++ posId
     let outPath = maybe target id outSpec 
-    liftIO $ writeJannoFile outPath newJanno
+    liftIO $ writeJannoFile outPath (JannoRows newJanno)
 
 
-mergeRow :: JannoRow -> JannoRow -> [String] -> Bool -> JannoRow
+mergeRow :: (MonadThrow m) => JannoRow -> JannoRow -> [String] -> Bool -> m JannoRow
 mergeRow targetRow sourceRow fields overwrite = do
-    let targetRowRecord = toNamedRecord targetRow
-        sourceRowRecord = toNamedRecord sourceRow
-    fromNamedRecord $ HM.unionWith mergeIfMissing targetRowRecord sourceRowRecord
+    let targetRowRecord = Csv.toNamedRecord targetRow
+        sourceRowRecord = Csv.toNamedRecord sourceRow
+        parseResult = Csv.runParser . Csv.parseNamedRecord $ HM.unionWithKey mergeIfMissing targetRowRecord sourceRowRecord
+    case parseResult of
+        Left err -> throwM $ PoseidonGenericException $ "Janno row-merge error: " ++ err
+        Right r  -> return r
   where
-    mergeIfMissing targetVal sourceVal = if targetVal `elem` ["n/a", ""] || overwrite then sourceVal else targetVal
+    mergeIfMissing key targetVal sourceVal =
+        if (null key || (BSC.unpack key `elem` fields)) && (targetVal `elem` ["n/a", ""] || overwrite) then
+            sourceVal
+        else
+            targetVal
