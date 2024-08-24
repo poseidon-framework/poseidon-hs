45326b323f467689b9b154faf10f366fccd4389a
Clemens Schmid <clemens@nevrome.de>
2024-08-24 15:47:55 +0200

    started to move janno types to an own module - they take a lot of real estate

--- src/Poseidon/Janno.hs
+++ src/Poseidon/Janno.hs
@@ -52,6 +52,7 @@ import           Poseidon.Utils                       (PoseidonException (..),
                                                        PoseidonIO, logDebug,
                                                        logError, logWarning,
                                                        renderPoseidonException)
+import Poseidon.JannoTypes
 
 import           Control.Exception                    (throwIO)
 import           Control.Monad                        (unless, when)
@@ -91,53 +92,6 @@ import           SequenceFormats.Eigenstrat           (EigenstratIndEntry (..),
 import qualified Text.Parsec                          as P
 import qualified Text.Parsec.String                   as P
 import qualified Text.Regex.TDFA                      as Reg
-import qualified Data.ByteString as S
-
-parseType :: MonadFail m => String -> (T.Text -> m a) -> S.ByteString -> m a
-parseType colName constructor x = case T.decodeUtf8' x of
-        Left e  -> fail $ show e ++ " in column " ++ colName
-        Right t -> constructor t
-
--- | A datatype for the Genetic_Sex .janno column
-newtype JannoSex = JannoSex { sfSex :: Sex }
-    deriving (Eq)
-
-makeJannoSex :: MonadFail m => String -> m JannoSex
-makeJannoSex x
-    | x == "F"  = pure (JannoSex Female)
-    | x == "M"  = pure (JannoSex Male)
-    | x == "U"  = pure (JannoSex Unknown)
-    | otherwise = fail $ "Genetic_Sex is set to " ++ show x ++ ". That is not in the allowed set [F, M, U]."
-
-instance Show JannoSex where
-    show (JannoSex Female)  = "F"
-    show (JannoSex Male)    = "M"
-    show (JannoSex Unknown) = "U"
-instance Ord JannoSex where
-    compare (JannoSex Female) (JannoSex Male)    = GT
-    compare (JannoSex Male) (JannoSex Female)    = LT
-    compare (JannoSex Male) (JannoSex Unknown)   = GT
-    compare (JannoSex Unknown) (JannoSex Male)   = LT
-    compare (JannoSex Female) (JannoSex Unknown) = GT
-    compare (JannoSex Unknown) (JannoSex Female) = LT
-    compare _ _                                  = EQ
-instance Csv.ToField JannoSex where   toField x    = Csv.toField $ show x
-instance Csv.FromField JannoSex where parseField x = Csv.parseField x >>= makeJannoSex
-instance ToJSON JannoSex where        toJSON x     = String $ T.pack $ show x
-instance FromJSON JannoSex where      parseJSON    = withText "JannoSex" (makeJannoSex . T.unpack)
-
--- | A datatype for the Alternative_IDs .janno column
-newtype JannoAlternativeID = JannoAlternativeID T.Text
-    deriving (Eq)
-
-makeJannoAlternativeID :: MonadFail m => T.Text -> m JannoAlternativeID
-makeJannoAlternativeID x = pure $ JannoAlternativeID x
-
-instance Show JannoAlternativeID where          show (JannoAlternativeID x)  = T.unpack x
-instance Csv.ToField JannoAlternativeID where   toField (JannoAlternativeID x) = Csv.toField x
-instance Csv.FromField JannoAlternativeID where parseField = parseType "Alternative_IDs" makeJannoAlternativeID
-instance ToJSON JannoAlternativeID where        toJSON (JannoAlternativeID x) = String x
-instance FromJSON JannoAlternativeID where      parseJSON = withText "Alternative_IDs" makeJannoAlternativeID
 
 -- | A datatype for BC-AD ages
 newtype BCADAge =
