256b3b32597b49ef7072d9c57a4a94f51d3f424f
Clemens Schmid <clemens@nevrome.de>
2024-11-24 21:29:28 +0100

    command line documentation

--- src/Poseidon/CLI/OptparseApplicativeParsers.hs
+++ src/Poseidon/CLI/OptparseApplicativeParsers.hs
@@ -905,5 +905,5 @@ parseZipOut :: OP.Parser Bool
 parseZipOut = OP.switch (
     OP.long "zip" <>
     OP.short 'z' <>
-    OP.help "whether the resultung genotype- and snp-files should be gzipped"
+    OP.help "Whether the resulting genotype- and snp-files should be gzipped."
     )
