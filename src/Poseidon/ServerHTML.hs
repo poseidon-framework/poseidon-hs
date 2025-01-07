804976fb8f33d912cc720ca91538c1b3361ab53e
Clemens Schmid <nevromecs@gmail.com>
2025-01-07 13:32:34 +0100

    experiments with a small css framwork

--- src/Poseidon/ServerHTML.hs
+++ src/Poseidon/ServerHTML.hs
@@ -26,73 +26,87 @@ jscript = [text|
 
 |]
 
+headerWithCSS :: H.Markup
+headerWithCSS = H.head $ do
+    H.script ! A.type_ "text/javascript" $ H.text jscript
+    H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.blue.min.css"
+
 mainPage :: [String] -> S.ActionM ()
 mainPage archiveNames = S.html $ renderMarkup $ do
   H.html $ do
-    H.head $ do
-      H.script ! A.type_ "text/javascript" $ H.text jscript
+    headerWithCSS
     H.body $ do
-      H.h1 "Poseidon public archives"
-      H.ul $ mapM_ (\archiveName -> H.li $ H.div $ do
-              H.a ! A.href ("/" <> H.toValue archiveName) $
-                H.toMarkup archiveName
-          ) archiveNames
+      H.main $ do
+        H.h1 "Archives"
+        H.ul $ mapM_ (\archiveName -> do
+                H.article $ do
+                  H.header $ do
+                    H.a ! A.href ("/" <> H.toValue archiveName) $
+                      H.toMarkup archiveName
+                  H.toMarkup ("test" :: String)
+            ) archiveNames
 
 archivePage :: String -> [PoseidonPackage] -> S.ActionM ()
 archivePage archiveName pacs = S.html $ renderMarkup $ do
   H.html $ do
-    H.head $ do
-      H.script ! A.type_ "text/javascript" $ H.text jscript
+    headerWithCSS
     H.body $ do
-      H.h1 $ H.toMarkup archiveName
-      H.ul $ mapM_ (\pac -> H.li $ H.div $ do
-          let pacName = getPacName pac
-              pacVersion = getPacVersion pac
-          H.a ! A.href ("/" <>  H.toValue archiveName <> "/" <> H.toValue pacName) $
-            H.toMarkup pacName
-          H.toMarkup (" | " :: String)
-          H.a ! A.href ("/zip_file/" <> H.toValue pacName <> "?package_version=" <> H.toValue (renderMaybeVersion pacVersion)) $
-            H.toMarkup ("Download" :: String)
-        ) pacs
+      H.main $ do
+        H.h1 (H.toMarkup $ "Archive: " <> archiveName)
+        H.ul $ mapM_ (\pac -> H.li $ H.div $ do
+            let pacName = getPacName pac
+                pacVersion = getPacVersion pac
+            H.a ! A.href ("/" <>  H.toValue archiveName <> "/" <> H.toValue pacName) $
+              H.toMarkup pacName
+            H.toMarkup (" | " :: String)
+            H.a ! A.href ("/zip_file/" <> H.toValue pacName <> "?package_version=" <> H.toValue (renderMaybeVersion pacVersion)) $
+              H.toMarkup ("Download" :: String)
+          ) pacs
 
 packagePage :: String -> String -> [PoseidonPackage] -> S.ActionM ()
 packagePage archiveName pacName pacs = S.html $ renderMarkup $ do
   H.html $ do
+    headerWithCSS
     H.body $ do
-      H.h1 (H.toMarkup pacName)
-      H.ul $ mapM_ (\pac -> H.li $ H.div $ do
-           let version = getPacVersion pac
-           H.a ! A.href ("/" <> H.toValue archiveName <> "/" <> H.toValue pacName <> "/" <> H.toValue (renderMaybeVersion version)) $
-               H.toMarkup $ renderMaybeVersion version
-        ) pacs
+      H.main $ do
+        H.h1 (H.toMarkup $ "Package: " <> pacName)
+        H.ul $ mapM_ (\pac -> H.li $ H.div $ do
+             let version = getPacVersion pac
+             H.a ! A.href ("/" <> H.toValue archiveName <> "/" <> H.toValue pacName <> "/" <> H.toValue (renderMaybeVersion version)) $
+                 H.toMarkup $ renderMaybeVersion version
+          ) pacs
 
 packageVersionPage :: String -> String -> Version -> [JannoRow] -> S.ActionM ()
 packageVersionPage archiveName pacName pacVersion jannoRows = S.html $ renderMarkup $ do
   H.html $ do
+    headerWithCSS
     H.body $ do
-      H.h1 (H.toMarkup pacName)
-      H.ul $ mapM_ (\jannoRow -> H.li $ H.div $ do
-           H.a ! A.href ("/" <> H.toValue archiveName <> "/" <> H.toValue pacName <> "/" <> H.toValue (showVersion pacVersion) <> "/" <> H.toValue (jPoseidonID jannoRow)) $
-               H.toMarkup $ jPoseidonID jannoRow
-        ) jannoRows
+      H.main $ do
+        H.h1 (H.toMarkup $ "Package: " <> pacName <> "-" <> showVersion pacVersion)
+        H.ul $ mapM_ (\jannoRow -> H.li $ H.div $ do
+             H.a ! A.href ("/" <> H.toValue archiveName <> "/" <> H.toValue pacName <> "/" <> H.toValue (showVersion pacVersion) <> "/" <> H.toValue (jPoseidonID jannoRow)) $
+                 H.toMarkup $ jPoseidonID jannoRow
+          ) jannoRows
 
 samplePage :: JannoRow -> S.ActionM ()
 samplePage row = S.html $ renderMarkup $ do
   H.html $ do
+    headerWithCSS
     H.body $ do
-      H.h1 (H.toMarkup $ jPoseidonID row)
-      H.table $ do
-        H.tr $ do
-          H.th "Property"
-          H.th "Value"
-        H.tr $ do
-          H.td "PoseidonID"
-          H.td (H.toMarkup $ jPoseidonID row)
-        H.tr $ do
-          H.td "GeneticSex"
-          H.td (H.toMarkup $ show $ jGeneticSex row)
-        H.tr $ do
-          H.td "..."
-          H.td (H.toMarkup ("..." :: String))
+      H.main $ do
+        H.h1 (H.toMarkup $ "Sample: " <> jPoseidonID row)
+        H.table $ do
+          H.tr $ do
+            H.th "Property"
+            H.th "Value"
+          H.tr $ do
+            H.td "PoseidonID"
+            H.td (H.toMarkup $ jPoseidonID row)
+          H.tr $ do
+            H.td "GeneticSex"
+            H.td (H.toMarkup $ show $ jGeneticSex row)
+          H.tr $ do
+            H.td "..."
+            H.td (H.toMarkup ("..." :: String))
 
 
